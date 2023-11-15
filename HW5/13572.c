53e81dbe1a3aab7f3320ac8532c1866c4f55f91c
瘋狂挺腰獸 <56062462+Crazy-Ting-Yao@users.noreply.github.com>
2023-11-15 16:07:01 +0800

    Update 13572.c

--- HW5/13572.c
+++ HW5/13572.c
@@ -1,4 +1,4 @@
-//13573
+//13572
 #include<stdio.h>
 #include<string.h>
 int main(){
@@ -7,46 +7,21 @@ int main(){
         alph[i]='a'+i;
     }
     scanf("%s",c);
-    int len=strlen(c);
     int num;
     scanf("%d",&num);
     char r,t;
-    short ty;
-    int index;
     for(int i=0;i<num;i++){
-        scanf("%hd",&ty);
-        if(ty==1){
-            getchar();
-            r = getchar();
-            getchar();
-            t = getchar();
-            for(int j=0;j<26;j++){
-                if(alph[j]==r){
-                    alph[j]=t;
-                }
-            }    
-        }
-        else if(ty==2){
-            scanf("%d %c",&index,&t);
-            int flag=1;
-            for(int j=0;j<26;j++){
-                if(alph[j]==t){
-                    c[index]=j+'a';
-                    flag=0;
-                    break;
-                }
+        getchar(); 
+        r = getchar();
+        getchar();
+        t = getchar();
+        for(int j=0;j<26;j++){
+            if(alph[j]==r){
+                alph[j]=t;
             }
-            if(flag){
-                for(int k=0;k<len;k++){
-                    c[k]=alph[c[k]-'a'];
-                }
-                c[index]=t;
-                for(int i=0;i<26;i++){
-                    alph[i]='a'+i;
-                }
-            }
-        }
+        } 
     }
+    int len=strlen(c);
     for(int i=0;i<len;i++){
         printf("%c",alph[c[i]-'a']);
     }
